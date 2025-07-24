93d824dc07edd0a1f4afbee61069ac7e6005a476
youssef-laarossi <laarossi.youssef@gmail.com>
2025-07-24 12:20:52 -0400

    Rename module to `goload`, update Go version, and implement basic load test execution in `main.go`.

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module go-load
+module goload
 
-go 1.24
+go 1.22
--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	goload "goload/runner"
+	"time"
+)
+
+func main() {
+	config := goload.Config{
+		Method:        goload.GET,
+		URI:           "https://www.google.com",
+		UserAgent:     "PostmanRuntime/7.44.1",
+		LogOutputPath: "logs",
+		Vus:           2,
+		Execution: goload.Execution{
+			Duration: 2 * time.Second,
+		},
+	}
+
+	duration, err := time.ParseDuration("30s")
+	if err != nil {
+		panic(err)
+	}
+
+	config.Timeout = duration
+	goload.Execute(config)
+}
