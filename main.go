4257eaad46e8a13a4c4c02eec08535373343c6c3
youssef-laarossi <laarossi.youssef@gmail.com>
2025-08-22 23:25:48 -0400

    Refactor `main.go`: simplify configuration loading and execution flow with YAML-based setup.

--- main.go
+++ main.go
@@ -1,35 +1,28 @@
 package main
 
 import (
+	"fmt"
 	goload "goload/runner"
-	"time"
+	"gopkg.in/yaml.v3"
+	"os"
+	"path/filepath"
 )
 
 func main() {
-	config := goload.Config{
-		Request: goload.Request{
-			Method:    goload.GET,
-			URI:       "http://localhost:8974/products/100",
-			UserAgent: goload.ChromeAgent,
-		},
-		Response: goload.Response{
-			StatusCode: 200,
-			Body:       "{\"productID\":100,\"user\":{\"userID\":77,\"email\":\"oumaima@gmail.com\",\"firstName\":\"Mark\",\"lastName\":\"Charles\",\"address\":null,\"phoneNumber\":null,\"profilePicture\":\"26b67ab4-4e5b-42a6-8e41-0727508fb1ad.png\",\"gender\":null,\"city\":null,\"verified\":false,\"createdAt\":null,\"rating\":{\"rating\":5.0,\"numberOfRatings\":1}},\"category\":{\"categoryId\":0,\"name\":null,\"cover\":null},\"cities\":[{\"cityId\":20,\"countyCode\":\"CA\",\"name\":\"Moose Jaw\"}],\"sizes\":[{\"id\":59,\"categoryId\":8,\"sizeName\":\"XS\"}],\"materials\":[{\"id\":48,\"categoryId\":8,\"material\":\"Cotton\"}],\"brand\":null,\"name\":\"YSL Scarf 2\",\"description\":\"ede145\",\"condition\":\"like new\",\"price\":145.0,\"thumbnail\":\"197b70fbb5166ce7ce41995a540587ef-2910057047003213315-thumbnail.jpg\",\"genders\":[\"male\",\"female\"],\"images\":[\"197b70fbb5166ce7ce41995a540587ef-2910057047003213315.jpg\"],\"colors\":[\"ff03a9f4\",\"ffff5722\",\"ffffffff\"],\"favorite\":false,\"appContact\":true,\"negotiable\":false,\"phoneNumber\":null,\"active\":true,\"processing\":false,\"createdAt\":\"2024-12-15\"}",
-		},
-		LogOutputPath: "logs",
-		Timepoints: []goload.ExecutionTimepoint{
-			{
-				Duration: time.Second * 2,
-				TargetVu: 10,
-			},
-		},
-	}
-
-	duration, err := time.ParseDuration("30s")
+	configPath := filepath.Join("config", "smoke.yml")
+	yamlFile, err := os.ReadFile(configPath)
 	if err != nil {
-		panic(err)
+		fmt.Printf("Error reading YAML file: %s\n", err)
+		return
 	}
 
-	config.Timeout = duration
-	goload.Execute(config)
+	var collection goload.Collection
+	err = yaml.Unmarshal(yamlFile, &collection)
+	if err != nil {
+		fmt.Printf("Error parsing YAML: %s\n", err)
+		return
+	}
+
+	executor := goload.Executor{Collection: collection}
+	executor.Execute()
 }
