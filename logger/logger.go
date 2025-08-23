f72a6a226d16cf2e9a77b5773f4c4cd169a0627b
youssef-laarossi <laarossi.youssef@gmail.com>
2025-08-22 23:10:21 -0400

    Add `logger` package with basic logging functionality

---
+++ logger/logger.go
@@ -0,0 +1,36 @@
+package logger
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+type Logger struct {
+	Dateformat string
+	input      io.Writer
+}
+
+func NewLogger(logDir string) (Logger, error) {
+	logger := Logger{
+		Dateformat: "2006-01-02 15:04:05",
+		input:      nil,
+	}
+	fileName := logDir + "/log-" + time.Now().Format(logger.Dateformat) + ".log"
+	file, err := os.Create(fileName)
+	if err != nil {
+		return logger, fmt.Errorf("error creating log file: %s", err)
+	}
+	logger.input = io.Writer(file)
+	return logger, nil
+}
+
+func (logger *Logger) Log(logData string) error {
+	logData = time.Now().Format(logger.Dateformat) + " " + logData + "\n"
+	_, err := logger.input.Write([]byte(logData))
+	if err != nil {
+		return err
+	}
+	return nil
+}
