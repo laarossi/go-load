9db96bee0821db5b8be8c78366b5a4cf620c126e
Youssef Laarossi <laarossi.youssef@gmail.com>
2025-08-25 17:02:59 -0400

    BREAKING CHANGE: refactored http_client

--- client/http_client.go
+++ client/http_client.go
@@ -1,22 +1,23 @@
 package client
 
 import (
-	"bytes"
 	"goload/metrics"
 	"io"
 	"net/http"
+	"net/url"
+	"strings"
 	"time"
 )
 
 type HttpMethod string
 
 const (
-	GET    HttpMethod = "get"
-	POST   HttpMethod = "post"
-	PUT    HttpMethod = "put"
-	DELETE HttpMethod = "delete"
-	HEAD   HttpMethod = "head"
-	PATCH  HttpMethod = "patch"
+	GET    HttpMethod = "GET"
+	POST   HttpMethod = "POST"
+	PUT    HttpMethod = "PUT"
+	DELETE HttpMethod = "DELETE"
+	HEAD   HttpMethod = "HEAD"
+	PATCH  HttpMethod = "PATCH"
 )
 
 type UserAgent string
@@ -32,30 +33,44 @@ const (
 	IOSAgent     UserAgent = "ios"
 )
 
-type Response struct {
-	StatusCode int `yaml:"status_code,omitempty"`
-	Body       string
-	Duration   time.Duration
-	Headers    []RequestHeader `yaml:"headers,omitempty"`
-	Cookies    []RequestCookie `yaml:"cookies,omitempty"`
-	Error      error
+type HTTPResponse struct {
+	StatusCode    int `yaml:"status_code,omitempty"`
+	Body          string
+	Headers       []HTTPClientHeader `yaml:"headers,omitempty"`
+	Cookies       []HTTPClientCookie `yaml:"cookies,omitempty"`
+	RequestMetric *metrics.RequestMetric
+	NetworkMetric *metrics.NetworkMetric
+	Error         error
 }
 
-type Request struct {
-	Method    HttpMethod      `yaml:"method"`
-	URI       string          `yaml:"uri"`
-	UserAgent UserAgent       `yaml:"user_agent"`
-	Headers   []RequestHeader `yaml:"headers"`
-	Body      string          `yaml:"body"`
-	Cookies   []RequestCookie `yaml:"cookies"`
+type HTTPRequest struct {
+	Method    HttpMethod         `yaml:"method"`
+	URI       string             `yaml:"uri"`
+	UserAgent UserAgent          `yaml:"user_agent"`
+	Headers   []HTTPClientHeader `yaml:"headers"`
+	Body      string             `yaml:"body"`
+	Cookies   []HTTPClientCookie `yaml:"cookies"`
 }
 
-type RequestHeader struct {
+type HTTPClientHeader struct {
 	Name  string `yaml:"name"`
 	Value string `yaml:"value"`
 }
 
-type RequestCookie struct {
+func (requestHeader *HTTPClientHeader) convertToHeader() http.Header {
+	header := http.Header{}
+	header.Set(requestHeader.Name, requestHeader.Value)
+	return header
+}
+
+func (requestHeader *HTTPClientHeader) parse(header http.Header) HTTPClientHeader {
+	return HTTPClientHeader{
+		Name:  requestHeader.Name,
+		Value: header.Get(requestHeader.Name),
+	}
+}
+
+type HTTPClientCookie struct {
 	Name       string    `yaml:"name"`
 	Value      string    `yaml:"value"`
 	Path       string    `yaml:"path"`
@@ -69,129 +84,132 @@ type RequestCookie struct {
 	RawMaxAge  string    `yaml:"raw_max_age"`
 }
 
+func (requestCookie *HTTPClientCookie) convertToCookie() http.Cookie {
+	return http.Cookie{
+		Name:       requestCookie.Name,
+		Value:      requestCookie.Value,
+		Path:       requestCookie.Path,
+		Domain:     requestCookie.Domain,
+		Expires:    requestCookie.Expires,
+		RawExpires: requestCookie.RawExpires,
+		MaxAge:     requestCookie.MaxAge,
+	}
+}
+
+func (requestCookie *HTTPClientCookie) parse(cookie http.Cookie) HTTPClientCookie {
+	return HTTPClientCookie{
+		Name:       cookie.Name,
+		Value:      cookie.Value,
+		Path:       cookie.Path,
+		Domain:     cookie.Domain,
+		Expires:    cookie.Expires,
+		RawExpires: cookie.RawExpires,
+		MaxAge:     cookie.MaxAge,
+	}
+}
+
 type Client struct {
 	HttpClient *http.Client
 }
 
-func parseSameSite(sameSite string) http.SameSite {
-	switch sameSite {
-	case "lax":
-		return http.SameSiteLaxMode
-	case "strict":
-		return http.SameSiteStrictMode
-	case "none":
-		return http.SameSiteNoneMode
-	default:
-		return http.SameSiteDefaultMode
-	}
-}
-
 type RequestOptions struct {
-	Headers []RequestHeader
-	Cookies []RequestCookie
+	Headers []HTTPClientHeader
+	Cookies []HTTPClientCookie
 	Body    *string
 }
 
-// DoRequest performs any HTTP method with headers, cookies, and body
-func (c *Client) DoRequest(method, url string, opts *RequestOptions) (*metrics.RequestMetric, *metrics.NetworkMetric, error) {
+func (c *Client) ExecuteRequest(req *http.Request) (*HTTPResponse, error) {
 	startTime := time.Now()
-
-	var bodyReader io.Reader
-	var sentBytes int64
-	if opts != nil && opts.Body != nil {
-		sentBytes = int64(len(*opts.Body))
-		bodyReader = bytes.NewReader([]byte(*opts.Body))
-	}
-
-	req, err := http.NewRequest(method, url, bodyReader)
+	resp, err := c.HttpClient.Do(req)
 	if err != nil {
-		return nil, nil, err
+		return &HTTPResponse{
+			Error: err,
+			RequestMetric: &metrics.RequestMetric{
+				Duration: time.Since(startTime),
+			},
+		}, err
+	}
+	defer resp.Body.Close()
+
+	// Read response body
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return &HTTPResponse{
+			StatusCode: resp.StatusCode,
+			Error:      err,
+			RequestMetric: &metrics.RequestMetric{
+				Duration:   time.Since(startTime),
+				StatusCode: resp.StatusCode,
+			},
+		}, err
 	}
 
-	// Add headers
-	if opts != nil && opts.Headers != nil {
-		for _, v := range opts.Headers {
-			req.Header.Set(v.Name, v.Value)
-		}
-	}
+	endTime := time.Now()
+	duration := endTime.Sub(startTime)
 
-	// Add cookies
-	if opts != nil && opts.Cookies != nil {
-		for _, cookie := range opts.Cookies {
-			req.AddCookie(&http.Cookie{
-				Name:       cookie.Name,
-				Value:      cookie.Value,
-				Path:       cookie.Path,
-				Domain:     cookie.Domain,
-				Expires:    cookie.Expires,
-				RawExpires: cookie.RawExpires,
-				MaxAge:     cookie.MaxAge,
-				Secure:     cookie.Secure,
-				HttpOnly:   cookie.HTTPOnly,
-				SameSite:   parseSameSite(cookie.SameSite),
-				Raw:        "",
-				Unparsed:   nil,
+	// Convert headers
+	var headers []HTTPClientHeader
+	for name, values := range resp.Header {
+		for _, value := range values {
+			headers = append(headers, HTTPClientHeader{
+				Name:  name,
+				Value: value,
 			})
 		}
 	}
 
-	resp, err := c.HttpClient.Do(req)
-	if err != nil {
-		return nil, nil, err
+	// Convert cookies
+	var cookies []HTTPClientCookie
+	for _, cookie := range resp.Cookies() {
+		cookies = append(cookies, HTTPClientCookie{
+			Name:       cookie.Name,
+			Value:      cookie.Value,
+			Path:       cookie.Path,
+			Domain:     cookie.Domain,
+			Expires:    cookie.Expires,
+			RawExpires: cookie.RawExpires,
+			MaxAge:     cookie.MaxAge,
+			Secure:     cookie.Secure,
+			HTTPOnly:   cookie.HttpOnly,
+		})
 	}
-	defer resp.Body.Close()
 
-	// Read all response bytes
-	responseBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	requestMetric := &metrics.RequestMetric{
-		Duration:   time.Since(startTime),
+	return &HTTPResponse{
 		StatusCode: resp.StatusCode,
-	}
-
-	networkMetric := &metrics.NetworkMetric{
-		BytesSent: sentBytes,
-		BytesRecv: int64(len(responseBytes)),
-	}
-
-	return requestMetric, networkMetric, nil
+		Body:       string(bodyBytes),
+		Headers:    headers,
+		Cookies:    cookies,
+		RequestMetric: &metrics.RequestMetric{
+			Duration:   duration,
+			StatusCode: resp.StatusCode,
+		},
+		NetworkMetric: &metrics.NetworkMetric{
+			BytesSent: 0,
+			BytesRecv: int64(len(bodyBytes)),
+		},
+		Error: nil,
+	}, nil
 }
 
-// Convenience methods
-func (c *Client) Get(url string, opts *RequestOptions) (*metrics.RequestMetric, *metrics.NetworkMetric, error) {
-	return c.DoRequest(http.MethodGet, url, opts)
-}
-
-func (c *Client) Post(url string, contentType string, body []byte, opts *RequestOptions) (*metrics.RequestMetric, *metrics.NetworkMetric, error) {
-	if opts == nil {
-		opts = &RequestOptions{}
-	}
-	if opts.Headers == nil {
-		opts.Headers = make([]RequestHeader, 0)
+func CreateRequest(request HTTPRequest) (*http.Request, error) {
+	parsedURL, err := url.Parse(request.URI)
+	if err != nil {
+		return nil, err
 	}
 
-	opts.Headers = append(opts.Headers, RequestHeader{Name: "Content-Type", Value: contentType})
-	s := string(body)
-	opts.Body = &s
-	return c.DoRequest(http.MethodPost, url, opts)
-}
-
-func (c *Client) Put(url string, contentType string, body []byte, opts *RequestOptions) (*metrics.RequestMetric, *metrics.NetworkMetric, error) {
-	if opts == nil {
-		opts = &RequestOptions{}
+	headers := http.Header{}
+	for _, header := range request.Headers {
+		headers.Add(header.Name, header.Value)
 	}
-	if opts.Headers == nil {
-		opts.Headers = make([]RequestHeader, 0)
-	}
-	opts.Headers = append(opts.Headers, RequestHeader{Name: "Content-Type", Value: contentType})
-	s := string(body)
-	opts.Body = &s
-	return c.DoRequest(http.MethodPut, url, opts)
-}
 
-func (c *Client) Delete(url string, opts *RequestOptions) (*metrics.RequestMetric, *metrics.NetworkMetric, error) {
-	return c.DoRequest(http.MethodDelete, url, opts)
+	req := &http.Request{
+		Method: "GET",
+		URL:    parsedURL,
+		Body:   io.NopCloser(strings.NewReader(request.Body)),
+		Header: headers,
+		Host:   parsedURL.Host,
+		Proto:  "HTTP/1.1",
+	}
+
+	return req, nil
 }
