6314311b4a631ed0e203bf232d55f0d9884b6ef9
Youssef Laarossi <laarossi.youssef@gmail.com>
2025-08-25 16:59:06 -0400

    =rename client.Request to client.HTTPRequest

--- runner/config.go
+++ runner/config.go
@@ -11,25 +11,25 @@ type Collection struct {
 }
 
 type Test struct {
-	Name    *string        `yaml:"name"`
-	Global  *Global        `yaml:"global,omitempty"`
-	Request client.Request `yaml:"request"`
-	Phases  []Phase        `yaml:"phases"`
+	Name    *string            `yaml:"name"`
+	Global  *Global            `yaml:"global,omitempty"`
+	Request client.HTTPRequest `yaml:"request"`
+	Phases  []Phase            `yaml:"phases"`
 }
 type Global struct {
-	Timeout      time.Duration `yaml:"timeout,omitempty"` // e.g., "30s"
-	Retries      int           `yaml:"retries,omitempty"` // Number of retries per request
-	RetriesDelay time.Duration `yaml:"retries_delay,omitempty"`
-	ThinkTime    time.Duration `yaml:"think_time,omitempty"` // Delay between requests per VU
+	Timeout      time.Duration  `yaml:"timeout,omitempty"` // e.g., "30s"
+	Retries      int            `yaml:"retries,omitempty"` // Number of retries per request
+	RetriesDelay time.Duration  `yaml:"retries_delay,omitempty"`
+	ThinkTime    *time.Duration `yaml:"think_time,omitempty"` // Delay between requests per VU
 }
 type Phase struct {
-	Name          *string         `yaml:"name"`
-	SingleRequest *bool           `yaml:"single_request,omitempty"`
-	Duration      *time.Duration  `yaml:"duration,omitempty"`
-	Increment     *time.Duration  `yaml:"increment,omitempty"`
-	IncrementVus  *int            `yaml:"increment_vus,omitempty"`
-	TargetVUs     *int            `yaml:"target_vus,omitempty"`
-	Request       *client.Request `yaml:"request"`
+	Name          *string             `yaml:"name"`
+	SingleRequest *bool               `yaml:"single_request,omitempty"`
+	Duration      *time.Duration      `yaml:"duration,omitempty"`
+	Increment     *time.Duration      `yaml:"increment,omitempty"`
+	IncrementVus  *int                `yaml:"increment_vus,omitempty"`
+	TargetVUs     *int                `yaml:"target_vus,omitempty"`
+	Request       *client.HTTPRequest `yaml:"request"`
 }
 
 type LogConfig struct {
